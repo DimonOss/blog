d3b71813663b7fca73e582664ef6121a0d1c9a81
DimonOss <dmytro.sukhovolets@oa.edu.ua>
2021-05-12 23:25:04 +0300

    lab_1-10

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +18,18 @@ use App\Http\Controllers\RestTestController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'digging_deeper'], function () {
 
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
+});
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
     Route::resource('rest', RestTestController::class)->names('restTest');
 })->name('dashboard');
+
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
@@ -30,10 +38,15 @@ $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
+
 Route::group($groupData, function () {
     //BlogCategory
     $methods = ['index','edit','store','update','create',];
     Route::resource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+    //BlogPost
+    Route::resource('posts', PostController::class)
+        ->except(['show'])                               //не робити маршрут для метода show
+        ->names('blog.admin.posts');
 });
